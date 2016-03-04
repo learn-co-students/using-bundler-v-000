25ef9f0f8dfb3b360f55a1ed0585c02b235974fb
agdavid <antonio.g.david@gmail.com>
2016-03-04 14:44:02 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,8 @@
+#The bin/run file starts the app.
+#The bin/run file requires the config/environment file to provide access to Gems
+require_relative '../config/environment.rb'
+
+
 Hashie
 Sinatra
 Octokit
---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+#The environment file is where you load all the app's dependencies
+#These include Gems and database connections.
+require 'bundler/setup'
+Bundler.require(:default, :development)
