dda797a5fc5e042909084264a0c62b0b165128d2
tpstar <hanlee12@gmail.com>
2016-06-03 14:37:05 +0000

    code using-bundler

--- spec/bundler_spec.rb
+++ spec/bundler_spec.rb
@@ -49,7 +49,7 @@ describe "Bundler" do
 
       # http://bundler.io/v1.3/groups.html
       it "should contain the pry gem in the development group using a hash argument to the gem method" do
-        expect(@gemfile_text =~ /gem .pry.,.*group.*development'?/).not_to eq(nil)
+        expect(@gemfile_text =~ /group .*development.* do/).not_to eq(nil)
         expect(@bundle_output =~ /pry/).not_to eq(nil)
 
         bundle_output_without_development = ""
