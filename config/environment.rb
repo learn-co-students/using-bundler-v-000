f9466687c40b7fe6dc8dd1af55f0c91a13a70828
Aziz Kosber <a.kosber@gmail.com>
2018-08-08 11:52:55 +0000

    Done.

---
+++ config/environment.rb
@@ -0,0 +1,2 @@
+require 'bundler/setup' #this is a command that tells Ruby, "hey, we are using bundler and thats where all our gems are referenced. go look up the gems over there."
+Bundler.require(:default, :development)
