ecd24b4df04e3de6b0adff894a1b8c18073dc705
Aziz Kosber <a.kosber@gmail.com>
2018-08-19 02:33:58 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -11,4 +11,18 @@ group :test do
   gem "rspec"
 end
 
-# git@github.com:awesome-print/awesome_print.git
+
+# source "https://rubygems.org"
+#
+# gem "sinatra", "1.4.4"
+# gem "hashie"
+# gem "octokit", "~>2.0"
+# gem "awesome_print", :git => "git@github.com:awesome-print/awesome_print.git"
+#
+# gem "pry", :group => "development"
+#
+# group :test do
+#   gem "rspec"
+# end
+#
+# # git@github.com:awesome-print/awesome_print.git
--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,8 @@
-require 'bundler/setup' #this is a command that tells Ruby, "hey, we are using bundler and thats where all our gems are referenced. go look up the gems over there."
+require 'bundler/setup'
 Bundler.require(:default, :development)
+
+
+
+
+# require 'bundler/setup' #this is a command that tells Ruby, "hey, we are using bundler and thats where all our gems are referenced. go look up the gems over there."
+# Bundler.require(:default, :development)
